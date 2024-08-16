7f204052e48b33b3d05cb06d0f20ec1df724e31e
Lenny <100688028+Isasanelli@users.noreply.github.com>
2024-08-16 23:59:22 +0200

    finish

--- scripts/knowledge_Base.py
+++ scripts/knowledge_Base.py
@@ -5,24 +5,26 @@ def create_ontology():
     g = rdflib.Graph()
     baseDir = os.path.dirname(os.path.abspath(__file__))
     
-    # Definizione di un namespace realistico per Amazon Prime
-    amazon_ns = rdflib.Namespace("http://amazon.com/prime#")
+    # Definizione dei namespace per DBpedia e schema.org
+    dbpedia_ns = rdflib.Namespace("http://dbpedia.org/ontology/")
     schema_ns = rdflib.Namespace("http://schema.org/")
     
     # Dichiarazione delle classi e proprietà
-    g.add((amazon_ns.Movie, rdflib.RDFS.subClassOf, schema_ns.CreativeWork))
-    g.add((amazon_ns.TVShow, rdflib.RDFS.subClassOf, schema_ns.CreativeWork))
-    g.add((amazon_ns.Actor, rdflib.RDFS.subClassOf, schema_ns.Person))
-    g.add((amazon_ns.Director, rdflib.RDFS.subClassOf, schema_ns.Person))
+    g.add((dbpedia_ns.Film, rdflib.RDFS.subClassOf, schema_ns.CreativeWork))
+    g.add((dbpedia_ns.TelevisionShow, rdflib.RDFS.subClassOf, schema_ns.CreativeWork))
+    g.add((dbpedia_ns.Person, rdflib.RDFS.subClassOf, schema_ns.Person))
     
-    # Dichiarazione delle proprietà utilizzando proprietà simili a quelle di Schema.org
-    g.add((amazon_ns.directedBy, rdflib.RDF.type, rdflib.OWL.ObjectProperty))
-    g.add((amazon_ns.directedBy, rdflib.RDFS.domain, amazon_ns.Movie))
-    g.add((amazon_ns.directedBy, rdflib.RDFS.range, amazon_ns.Director))
+    # Dichiarazione delle proprietà utilizzando proprietà di DBpedia
+    g.add((dbpedia_ns.director, rdflib.RDF.type, rdflib.OWL.ObjectProperty))
+    g.add((dbpedia_ns.director, rdflib.RDFS.domain, dbpedia_ns.Film))
+    g.add((dbpedia_ns.director, rdflib.RDFS.range, dbpedia_ns.Person))
     
-    g.add((amazon_ns.starring, rdflib.RDF.type, rdflib.OWL.ObjectProperty))
-    g.add((amazon_ns.starring, rdflib.RDFS.domain, amazon_ns.Movie))
-    g.add((amazon_ns.starring, rdflib.RDFS.range, amazon_ns.Actor))
+    g.add((dbpedia_ns.starring, rdflib.RDF.type, rdflib.OWL.ObjectProperty))
+    g.add((dbpedia_ns.starring, rdflib.RDFS.domain, dbpedia_ns.Film))
+    g.add((dbpedia_ns.starring, rdflib.RDFS.range, dbpedia_ns.Person))
+    
+    g.add((dbpedia_ns.starring, rdflib.RDFS.domain, dbpedia_ns.TelevisionShow))
+    g.add((dbpedia_ns.starring, rdflib.RDFS.range, dbpedia_ns.Person))
     
     # Definisce il percorso di output per salvare l'ontologia
     output_dir = os.path.join(baseDir, '..', 'results', 'models', 'knowledge_base')
